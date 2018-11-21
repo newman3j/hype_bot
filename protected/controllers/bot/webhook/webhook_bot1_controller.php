e69e4fc4e3d8fb1f071c23056e07db1b93ece0e2
novichkovv <novichkovv@bk.ru>
2018-11-21 21:54:34 +0800

    no message

--- protected/controllers/bot/webhook/webhook_bot1_controller.php
+++ protected/controllers/bot/webhook/webhook_bot1_controller.php
@@ -6,8 +6,7 @@ class webhook_bot1_controller extends bot_project
 		$this->writeLog('test', $_GET);
 		$this->writeLog('test', $_POST);
 		$raw = file_get_contents('php://input');
-		parse_str($raw, $params);
-		$this->writeLog('test', json_decode(json_decode($raw, true), true));
+		$this->writeLog('test', $raw);
 		$this->success();
 	}
 }
