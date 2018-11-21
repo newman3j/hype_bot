2e36a749ee05ae88b7dde601e51ab3938bcb606b
novichkovv <novichkovv@bk.ru>
2018-11-21 21:30:15 +0800

    webhook test

---
+++ protected/controllers/bot/webhook/webhook_bot1_controller.php
@@ -0,0 +1,13 @@
+<?php
+class webhook_bot1_controller extends bot_project
+{
+	public function end()
+	{
+		$this->writeLog('test', $_GET);
+		$this->writeLog('test', $_POST);
+		$raw = file_get_contents('php://input');
+		parse_str($raw, $params);
+		$this->writeLog('test', $params);
+		$this->success();
+	}
+}
