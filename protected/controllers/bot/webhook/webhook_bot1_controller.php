4f73985cebb4e11e7b1ecc5ba389d0aaa4ddceda
novichkovv <novichkovv@bk.ru>
2018-11-21 21:49:42 +0800

    strange thing

--- protected/controllers/bot/webhook/webhook_bot1_controller.php
+++ protected/controllers/bot/webhook/webhook_bot1_controller.php
@@ -7,7 +7,7 @@ class webhook_bot1_controller extends bot_project
 		$this->writeLog('test', $_POST);
 		$raw = file_get_contents('php://input');
 		parse_str($raw, $params);
-		$this->writeLog('test', json_decode($raw, true));
+		$this->writeLog('test', json_decode(json_decode($raw, true), true));
 		$this->success();
 	}
 }
